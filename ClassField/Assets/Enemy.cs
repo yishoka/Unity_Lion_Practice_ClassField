0e42aa808336f9a3436d770ee4a2f7eaac334a41
yishoka <63327438+yishoka@users.noreply.github.com>
2020-04-11 19:36:48 +0800

    練習作業0408

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+/// <summary>
+/// 怪物屬性
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    /// <summary>
+    /// 怪物的血量
+    /// </summary>
+    [Header("血量"),Tooltip("怪物的血量"),Range(50,500)]
+    public int HP;
+
+    /// <summary>
+    /// 怪物的魔力
+    /// </summary>
+    [Header("魔力"),Tooltip("怪物的魔力"),Range(10,100)]
+    public int MP = 100;
+
+    /// <summary>
+    /// 怪物的移動速度
+    /// </summary>
+    [Header("能力值"),Range(1,50.5f)]
+    public float speed = 10.5f;
+
+    /// <summary>
+    /// 怪物的攻擊力
+    /// </summary>
+    [Range(20,200)]
+    public int ATK = 50;
+
+    /// <summary>
+    /// 怪物的防禦力
+    /// </summary>
+    [Range(0,100)]
+    public int DEF;
+
+    /// <summary>
+    /// 怪物的武器名稱
+    /// </summary>
+    [Header("裝備")]
+    public string Weapon;
+
+    /// <summary>
+    /// 怪物的裝備名稱
+    /// </summary>
+    public string Equip = "皮衣";
+
+    /// <summary>
+    /// 怪物是否帶有鑰匙
+    /// </summary>
+    [Header("其他資料"),Tooltip("怪物是否帶有鑰匙")]
+    public bool Key = false;
+
+    /// <summary>
+    /// 怪物是否掉落寶物
+    /// </summary>
+    [Tooltip("怪物是否帶有寶物")]
+    public bool Treasure = false;
+
+}
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Enemy Slime1;
+    public Enemy Slime2;
+
+    public void Start()
+    {
+        print("史萊姆1號的魔力是：" + Slime1.MP);
+        print("史萊姆2號的裝備是：" + Slime2.Equip);
+
+        Slime1.HP = 100;
+        Slime2.HP = 300;
+        Slime1.DEF = 50;
+        Slime1.Weapon = "木棍";
+        Slime2.Weapon = "短刀";
+        Slime1.Key = true;
+        Slime2.Treasure = true;
+    }
+
+}
